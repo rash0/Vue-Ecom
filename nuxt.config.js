36bb1163c676db1f0760497b3661accdf20b0e27
Amr D <dabees.amr@gmail.com>
2023-05-14 17:06:57 +0300

    add package.json and nuxt/ts/npm config files

---
+++ nuxt.config.js
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  imports: {
+    dirs: ["store"],
+  },
+  modules: ["@nuxt/devtools", "@pinia/nuxt"],
+  pinia: {
+    autoImports: [
+      "defineStore", // import { defineStore } from 'pinia'
+      ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
+    ],
+  },
+  css: ["~/assets/styles/main.scss"],
+});
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    filenameHashing: false
-  }
